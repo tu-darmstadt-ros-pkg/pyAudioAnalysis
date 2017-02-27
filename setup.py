fbf5b8e60abef1dcfc86458002d9677aa56f0c91
Martin Oehler <martin.sven.oehler@gmail.com>
2017-02-27 11:22:42 +0100

    changed project to catkin project

--- setup.py
+++ setup.py
@@ -1,28 +1,22 @@
-try:
-	from setuptools import setup
-except ImportError:
-	from distutils.core import setup
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
 
-try:
-	from distutils.command.build_py import build_py_2to3 as build_py
-except ImportError:
-	from distutils.command.build_py import build_py
+#try:
+#    from distutils.command.build_py import build_py_2to3 as build_py
+#except ImportError:
+#    from distutils.command.build_py import build_py
 
-package = 'pyAudioAnalysis'
-version = '1.0'
-
-setup(name=package,
-	version=version,
-	packages = ['pyaudio_analysis','data'],
-        scripts = ['pyaudio_analysis/audioAnalysis.py'],
-	cmdclass={'build_py': build_py},
-	author='Theodoros Giannakopoulos',
-	author_email='tyiannak@gmail.com',
-	long_description=open('README.md').read(),
-	url='https://github.com/tyiannak/pyAudioAnalysis',
-	install_requires=[
-		"hmmlearn",
-		"simplejson",
-		"eyed3"
-	]
+setup_args = generate_distutils_setup(
+    packages = ['pyaudio_analysis','data'],
+    scripts = ['pyaudio_analysis/audioAnalysis.py'],
+#    cmdclass={'build_py': build_py},
+#    package_dir={'': 'src'},
+    long_description=open('README.md').read(),
+    install_requires=[
+        "hmmlearn",
+        "simplejson",
+        "eyed3"
+    ]
 )
+
+setup(**setup_args)
